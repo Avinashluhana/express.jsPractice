834d4e3cf4303b3db5a45bb663b88c3cbd73719b
avinashluhana <aviluhana@gmail.com>
2022-02-15 09:50:55 +0500

    changed the text

--- pugEngine.js
+++ pugEngine.js
@@ -1,6 +1,5 @@
 const express = require("express");
 
-
 // pug engine
 
 const app = express();
@@ -8,9 +7,10 @@ app.set("view engine", "pug");
 app.set("views", "./public/views");
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Hey", message: "Hello there!" });
+  res.render("index", {
+    title: "Geo",
+    message: "Zindgi dadi dukhi athai piyara",
+  });
 });
 
-
 app.listen(8000, () => console.log("server is running"));
-
