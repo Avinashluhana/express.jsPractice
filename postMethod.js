c32f1367dfaed797f456106caa187e11ab0c23b1
avinashluhana <aviluhana@gmail.com>
2022-03-16 01:51:58 +0500

    added data in api

--- postMethod.js
+++ postMethod.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
 
 const port = 5000;
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/form.html"));
 });
 
 app.post("/data", (req, res) => {
-  res.send("<h1>Data</h1>");
+    const userName= req.body.name;
+    const userEmail = req.body.email;
+    const userPass = req.body.password; 
+  res.json({
+
+    success: true,
+    name: userName,
+    email: userEmail,
+    password: userPass,
+
+  }
+    // `<h1>Data</h1>${req.body.name}${req.body.email}${req.body.password} `
+  );
 });
 
 app.listen(port, () => {
