780fbc1b7df810e6cac8e5e168e7ff2d17d28951
avinashluhana <aviluhana@gmail.com>
2022-03-19 01:58:37 +0500

    added router of about page

--- userRoutes.js
+++ userRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const registerUser = require("./userControl")
-const router = express.Router()
+const registerUser = require("./userControl");
+const router = express.Router();
 
-router.route("/data").post(registerUser).get((req,res)=> {
-    res.send("hale payo nah?")
+router
+  .route("/data")
+  .post(registerUser)
+  .get((req, res) => {
+    res.send("hale payo nah?");
+  });
+router.route("/data/about").get((req, res) => {
+  res.send("abour page?");
 });
-
 
 module.exports = router;
