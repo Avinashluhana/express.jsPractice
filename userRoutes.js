50a959e5c804afa627b2e529173711f7642ec1f3
avinashluhana <aviluhana@gmail.com>
2022-03-19 01:43:01 +0500

    added post + get method

--- userRoutes.js
+++ userRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const registerUser = require("./userControl")
 const router = express.Router()
 
-router.route("/data").post(registerUser);
+router.route("/data").post(registerUser).get((req,res)=> {
+    res.send("hale payo nah?")
+});
 
 
 module.exports = router;
