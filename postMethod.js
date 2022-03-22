81b8e43c4fd4a78524b3d6faf55a40a52d049db0
avinashluhana <aviluhana@gmail.com>
2022-03-23 00:54:16 +0500

    added another router

--- postMethod.js
+++ postMethod.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(router)
+app.use(router);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/form.html"));
