6b0879bfc32ed2edf36b9ff34e106b92db68e861
avinashluhana <aviluhana@gmail.com>
2022-03-28 21:45:51 +0500

    asdasdAS

--- postMethod.js
+++ postMethod.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const router = require("./userRoutes");
-const port = 5000;
+const port = 4000;
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
